995b3f5275c82a9fd90f26cfaa8e425dd45feb45
varixus <82593560+varixus@users.noreply.github.com>
2024-07-27 20:16:34 +0200

    Resolve issue #1

--- src/events/ready/onReady.js
+++ src/events/ready/onReady.js
@@ -1,11 +1,18 @@
 const { ActivityType } = require('discord.js');
 const getLocalCommands = require('../../utils/getLocalCommands');
+const Guild = require('../../mongooseModels/guildModel');
+const createGuild = require('../guildCreate/createGuild.js');
 
 module.exports = async (client) => {
     console.log(`${client.user.tag} is online.`);
 
     const guilds = client.guilds.cache.map(guild => guild);
     for(const guild of guilds){
+        if(await Guild.countDocuments( { guildID: guild.id} ) < 1){ // check if all guilds have database entry
+            console.log(guild.id + " not in database");
+            createGuild(client, guild);
+        }
+
         guild.commands.set(getLocalCommands());
     }
 
